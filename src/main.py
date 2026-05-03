71e86a026dcfc8c8740640729550f4e930d34a16
Sourovtalukder69 <sourovtalukder69@gmail.com>
2026-05-03 09:01:38 +0600

    playwright linkless navigation and html saving then parsing done

--- src/main.py
+++ src/main.py
@@ -4,24 +4,44 @@ from playwright.sync_api import sync_playwright
 
 with sync_playwright() as p:
     browser = p.chromium.launch(headless=False)
-    page = browser.new_page()
+    context = browser.new_context()
+    page = context.new_page()
 
     page.goto("https://www.charityextra.com/")
-
     page.wait_for_timeout(3000)
+
     page.get_by_role("button", name="Live Campaigns").click()
     page.wait_for_timeout(2000)
+
     page.wait_for_selector("text = Crowdfunders")
     page.click("text = Crowdfunders")
+
     page.wait_for_timeout(2000)
     page.mouse.wheel(0, 300)
     
-    for _ in range(1, random.randint(15, 20)):
+    # scrolling campaign lists
+    for _ in range(1, random.randint(7, 10)):
         scrool_amount = random.randint(200, 600)
         page.mouse.wheel(0, scrool_amount)
         time.sleep(random.uniform(0.5, 2))
+
     page.wait_for_timeout(3000)
-    html = page.content()
-    with open("data/Crowdfunders.html", "w", encoding="utf-8") as f:
-        f.write(html)
+
+    # all campaign navigating items
+    items = page.locator("div.grid.grid-cols-1.md\:grid-cols-2.lg\:grid-cols-3.xl\:grid-cols-4.min-\[2000px\]\:grid-cols-4.gap-5.md\:gap-6.mx-auto.w-full.max-w-\[1400px\].transition-opacity.duration-200 > div")
+    
+    for i in range(items.count()):
+        with page.expect_navigation():
+            items.nth(i).click()
+            page.wait_for_timeout(2000)
+            page.mouse.wheel(0, 400)
+            page.wait_for_timeout(3000)
+            html = page.content()
+            with open(f"data/page_number{i}.html", "w", encoding="utf-8") as f:
+                f.write(html)
+            page.go_back()
+            page.wait_for_timeout(3000)
+
+
+    page.wait_for_timeout(3000)
     page.close()
